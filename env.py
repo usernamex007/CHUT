e771f38753cc73d278d3088d17f97831746debb0
usernamex007 <sonalixsachin@gmail.com>
2025-02-08 05:15:18 +0530

    All Set !

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+API_ID = os.getenv("API_ID", "28795512").strip()
+API_HASH = os.getenv("API_HASH", "c17e4eb6d994c9892b8a8b6bfea4042a").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7610510597:AAFX2uCDdl48UTOHnIweeCMms25xOKF9PoA").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "postgres://iarfggbc:Vxzh_kG7cxa1kHR5faxcd1kuA4R-UT9E@rosie.db.elephantsql.com/iarfggbc").strip()
+MUST_JOIN = os.getenv("MUST_JOIN", "SANATANI_TECH")
 ALIVE_PIC = os.getenv("ALIVE_PIC", "https://telegra.ph/file/00eaed55184edf059dbf7.jpg")
 
 
