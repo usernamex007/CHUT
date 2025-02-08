2adf8175f6107bb8d906c847ed4b999f35a4822d
usernamex007 <sonalixsachin@gmail.com>
2025-02-08 05:41:19 +0530

    Update callbacks.py

--- StringSessionBot/callbacks.py
+++ StringSessionBot/callbacks.py
@@ -2,7 +2,7 @@ import traceback
 from data import Data
 from pyrogram import Client
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup
-from StringSessionBot.generate import generate_session, ask_ques, buttons_ques
+#from StringSessionBot.generate import generate_session, ask_ques, buttons_ques
 
 
 # Callbacks
@@ -42,34 +42,6 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.home_buttons),
         )
-    elif query == "generate":
-        await callback_query.answer()
-        await callback_query.message.reply(ask_ques, reply_markup=InlineKeyboardMarkup(buttons_ques))
-    elif query.startswith("pyrogram") or query.startswith("telethon"):
-        try:
-            if query == "pyrogram":
-                #await callback_query.answer("Please note that the new type of string sessions may not work in all bots, i.e, only the bots that have been updated to pyrogram v2 will work!", show_alert=True)
-                await generate_session(bot, callback_query.message)
-                """
-            # Maybe in future it'll come back.
-            elif query == "pyrogram1":
-                await callback_query.answer()
-                await generate_session(bot, callback_query.message, old_pyro=True)
-                """
-            elif query == "pyrogram_bot":
-                await callback_query.answer("Please note that this bot session will be of pyrogram v2", show_alert=True)
-                await generate_session(bot, callback_query.message, is_bot=True)
-            elif query == "telethon_bot":
-                await callback_query.answer()
-                await generate_session(bot, callback_query.message, telethon=True, is_bot=True)
-            elif query == "telethon":
-                await callback_query.answer()
-                await generate_session(bot, callback_query.message, telethon=True)
-        except Exception as e:
-            print(traceback.format_exc())
-            print(e)
-            await callback_query.message.reply(ERROR_MESSAGE.format(str(e)))
-
 
 ERROR_MESSAGE = "ᴏᴏᴘꜱ! ᴀɴ ᴇxᴄᴇᴘᴛɪᴏɴ ᴏᴄᴄᴜʀʀᴇᴅ! \ɴ\ɴ**ᴇʀʀᴏʀ** : {} " \
 "\ɴ\ɴᴘʟᴇᴀꜱᴇ ᴠɪꜱɪᴛ @V_VIP_OWNER ɪꜰ ᴛʜɪꜱ ᴍᴇꜱꜱᴀɢᴇ ᴅᴏᴇꜱɴ'ᴛ ᴄᴏɴᴛᴀɪɴ ᴀɴʏ " \
